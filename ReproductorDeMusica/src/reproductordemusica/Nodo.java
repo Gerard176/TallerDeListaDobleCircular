a79ad9cc30da38c52da6b45c3d307d297b37831a
Gerardo Duarte <gaduarter@ufpso.edu.co>
2023-05-03 16:24:43 -0500

    Creacion de la lista

---
+++ ReproductorDeMusica/src/reproductordemusica/Nodo.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reproductordemusica;
+
+/**
+ *
+ * @author MAELDUSA
+ */
+public class Nodo {
+    private String cancion;
+    private Nodo siguente;
+    private Nodo Anterior;
+
+    public String getCancion() {
+        return cancion;
+    }
+
+    public void setCancion(String cancion) {
+        this.cancion = cancion;
+    }
+
+    public Nodo getSiguente() {
+        return siguente;
+    }
+
+    public void setSiguente(Nodo siguente) {
+        this.siguente = siguente;
+    }
+
+    public Nodo getAnterior() {
+        return Anterior;
+    }
+
+    public void setAnterior(Nodo Anterior) {
+        this.Anterior = Anterior;
+    }
+    
+    
+}
---
+++ ReproductorDeMusica/src/reproductordemusica/Operaciones.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reproductordemusica;
+
+/**
+ *
+ * @author MAELDUSA
+ */
+public class Operaciones {
+    
+    Nodo cabeza;
+    Nodo cola;
+    String cancionEncontrada;
+    
+    public void insertar(String cancion){
+        Nodo nuevo = new Nodo();
+        nuevo.setCancion(cancion);
+        if (cabeza == null) {
+            cabeza = nuevo;
+            cola = nuevo;
+            cabeza.setSiguente(cola);
+            cola.setSiguente(cabeza);
+        }else{
+            
+            cabeza.setSiguente(nuevo);
+            nuevo.setAnterior(cabeza);
+            cola.setAnterior(nuevo);
+            nuevo.setSiguente(cola);
+            cabeza = nuevo;
+            
+        }
+    
+    }
+    public void buscar(String cancion){
+        Nodo actual = cola;
+        do {            
+            if (actual.getCancion().equals(cancion)) {
+                System.out.println("Cancion encontrada");
+                cancionEncontrada = actual.getCancion();
+            }
+            actual = actual.getSiguente();
+            
+        } while (actual.getSiguente() != cabeza);
+        
+    }
+    public void reproducirEnAleatorio(){
+        
+    }
+    
+}
